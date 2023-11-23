d95a95bab08a2a0a8f82094b98a97b4240095ffe
Douglas <portogdouglas@gmail.com>
2023-11-23 15:48:18 -0300

    Sign In (Autenticação)

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,37 @@
+const { compare } = require("bcryptjs")
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+const AuthConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
+
+class SessionController {
+  async create(req, res) {
+    const { email, password } = req.body
+
+    const user = await knex("users").where({ email }).first()
+
+    if (!user) {
+      throw new AppError('Usuário e/ou senha inválidos', 401)
+    }
+
+    if (password) {
+      const checkPassword = await compare(password, user.password)
+
+      if (!checkPassword) {
+        throw new AppError('Usuário e/ou senha inválidos', 401)
+      }
+    }
+
+    const { expiresIn, secret } = AuthConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return res.json({ user, token })
+  }
+}
+
+module.exports = SessionController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require("express")
 
 const usersRoutes = require('./users.routes')
+const sessionRoutesRoutes = require('./sessions.routes')
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionRoutesRoutes)
 
 module.exports=routes;
